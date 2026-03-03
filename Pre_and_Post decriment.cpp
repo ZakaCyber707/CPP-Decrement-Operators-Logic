44315f41de57a0ff5e0091868b9ebd302abf03db
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-03-04 00:27:18 +0500

    Implement pre and post decrement demonstrations
    "The code demonstrates how --a (pre-decrement) reduces the value before it is processed, while a-- (post-decrement) uses the current value before performing the subtraction."

---
+++ Pre_and_Post decriment.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a=5;
+	
+//	cout<<--a; //y (pre decrement) hy, pehly - kro phir value ko variable mn dal kr console mn show krwao
+				// 1st decrement than output
+	
+	
+	
+	cout<<a--<<endl; //y (post decrement) hy pehly (a) ko console pr hsow krwao phir agle bari mn value ko subtract kro 
+	cout<<a;	//1st output than decrement
+}
+
